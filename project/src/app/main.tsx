48b87a1a2c7b00bcc7dfbb63e12911bfa4b45223
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-01-27 20:57:17 +0900

    MainCSS 설정

--- project/src/app/main.tsx
+++ project/src/app/main.tsx
@@ -2,9 +2,11 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export const MainPage:React.FC = ()=>{
-    return <div>
-        <Link to="/mypage">
-            <button className="text-[60px] text-blue-600">hello welcome to ToHome</button>
-        </Link>
+    return (
+    <div>
+        <div className="w-[797px] h-[540px]">
+            <div className="fixed w-[797px] h-[540px] top-0 left-0 bg-[#ffffffb2] rounded-[52px]" />
+        </div>
     </div>
+    )
 }
