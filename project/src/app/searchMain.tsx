a5c394fa96687b7f7d02cf58d293dd04904c2a03
ye-one222 <101704967+ye-one222@users.noreply.github.com>
2024-02-16 23:13:17 +0900

    searchPage UI 수정

--- project/src/app/searchMain.tsx
+++ project/src/app/searchMain.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react"
-//import { Link } from "react-router-dom"
+import { recipes } from '../interface/recipes.tsx';
 import { Menu } from "../interface/menu.tsx";
+import { Link } from "react-router-dom";
 
 const FilterCard = () => {
-    const tags = ['플라스틱', '스티로폼', '캔', '비닐', '종이']; //나중에 더 추가
+    const tags = ['플라스틱', '스티로폼', '캔', '비닐', '종이', '유리']; //나중에 더 추가
     const [selectedTags, setSelectedTags] = useState<string[]>([]);
 
     return (
-        <div className="grid grid-cols-2 items-center w-[650px] h-[200px] bg-[#a0d4684c] rounded-[78px] p-6">
+        <div className="grid grid-cols-2 items-center w-[650px] h-[200px] bg-[#a0d4684c] rounded-[78px] p-8">
             {tags.map((tag, index) => {
                 return(
                     <div key={index} className="flex gap-1 text-[#507E1F]">
@@ -32,7 +33,7 @@ const FilterCard = () => {
     )
 }
 
-const RecipeCard = () => {
+const RecipeCard = ({ post_id, title, username }) => {
     /*const [imgUrl, setImgUrl] = useState('');
     const [title, setTitle] = useState('');
     const [user, setUser] = useState('');
@@ -40,37 +41,70 @@ const RecipeCard = () => {
     //fetch로 GET 요청 -> 각각 저장
 
     return (
+        <Link to={`/recipe/${post_id}`}>
         <div className="flex flex-col">
-            <div className="w-[230px] h-[230px] bg-[#f1f2f0] rounded-[20px]">
+            <div className="w-[230px] h-[230px] bg-[#f1f2f0] rounded-[20px] hover:scale-105 hover:shadow-2xl transition-transform ease-in-out duration-400">
                 {/* 사진 자리 - 나중에 이걸로 교체
                 <img src={imgUrl} alt="Photo" className="w-[245px] h-[245px] rounded-[20px]" />
                 */}
             </div>
             <div className="flex justify-between items-end">
-                <h1 className="w-[132px] text-[22px] text-black overflow-hidden">title</h1>
-                <div className="w-[80px] text-[18px] text-[#00000080] overflow-hidden">username</div>
+                <h1 className="w-[132px] text-[22px] text-left text-black overflow-hidden">{title}</h1>
+                <div className="w-[80px] text-[18px] text-right text-[#00000080] overflow-hidden">{username}</div>
             </div>
         </div>
+        </Link>
     )
 }
 
 export const SearchMainPage:React.FC = ()=>{
+    const tags = ['플라스틱', '스티로폼', '캔', '비닐', '종이', '유리']; //나중에 더 추가
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
     return (
     <div className="flex flex-col items-center">
         <Menu/>
 
         {/* 상단 필터 박스 */}
         <div className="flex flex-col mt-10 items-center gap-2">
-            <p className="text-[#507E1F] text-[15px]">레시피에 사용될 재료를 선택하세요.</p>
-            <FilterCard />
+            
+            
+            <div className="flex flex-col items-center w-[650px] bg-[#E9F3DE] rounded-[78px] px-8 py-4">
+                <p className="text-[#507E1F] text-[13px]">레시피에 사용될 재료를 선택하세요.</p>
+                <div className="mt-4 grid grid-cols-3 items-center gap-5">
+                    {tags.map((tag, index) => {
+                        return(
+                            <div key={index} className="flex gap-1 text-[#507E1F]">
+                                <input
+                                type="checkbox"
+                                className="w-5 h-5 accent-gray-50"
+                                onChange={({target: {checked}})=> {
+                                    if(checked){
+                                        setSelectedTags((prevSelectedTags) => [...prevSelectedTags, tag]);
+                                    }else{
+                                        setSelectedTags((prevSelectedTags) =>
+                                            prevSelectedTags.filter(atag => atag !== tag)
+                                        );
+                                    }
+                                }}
+                                />
+                                {tag}
+                            </div>
+                        )
+                    })}
+                </div>
+                <button className="mt-7 bg-[#a0d4684c] rounded-[10px] text-[#507E1F] text-[16px] w-[139px] h-[50px] border border-[#DEF0CA] hover:border-[#507e1f] transition-all">검색</button>
+            </div>
+
         </div>
 
         {/* 레시피들 3열 */}
         <div className="mt-5 grid grid-cols-3 gap-3">
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
+            {recipes.map((each, index) => {
+                return (
+                    <RecipeCard key={each.post_id} {...each}/>
+                )
+            })}
         </div>
     </div>
     )
